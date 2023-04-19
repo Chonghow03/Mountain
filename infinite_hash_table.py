96b3ddacac52e9f92cf5906369edb2b73023af84
veixhen <vxwilson@hotmail.com>
2023-04-20 02:04:22 +0800

    infinite table passed (TODO delete)

--- infinite_hash_table.py
+++ infinite_hash_table.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 from typing import Generic, TypeVar
+from data_structures.hash_table import LinearProbeTable, FullError
 
 from data_structures.referential_array import ArrayR
 
 K = TypeVar("K")
 V = TypeVar("V")
+
 
 class InfiniteHashTable(Generic[K, V]):
     """
@@ -20,13 +22,15 @@ class InfiniteHashTable(Generic[K, V]):
 
     TABLE_SIZE = 27
 
-    def __init__(self) -> None:
-        raise NotImplementedError()
+    def __init__(self, level=0) -> None:
+        self.array: ArrayR[tuple[K, V]] = ArrayR(self.TABLE_SIZE)
+        self.level = level
+        self.count = 0
 
     def hash(self, key: K) -> int:
         if self.level < len(key):
-            return ord(key[self.level]) % (self.TABLE_SIZE-1)
-        return self.TABLE_SIZE-1
+            return ord(key[self.level]) % (self.TABLE_SIZE - 1)
+        return self.TABLE_SIZE - 1
 
     def __getitem__(self, key: K) -> V:
         """
@@ -34,13 +38,55 @@ class InfiniteHashTable(Generic[K, V]):
 
         :raises KeyError: when the key doesn't exist.
         """
-        raise NotImplementedError()
+        try:
+            pos = self.get_location(key)
+        except KeyError:
+            raise KeyError('Key not found')
+        else:
+            table = self
+            for p in pos:
+                table = table.array[p][1]
+            return table
 
     def __setitem__(self, key: K, value: V) -> None:
         """
         Set an (key, value) pair in our hash table.
         """
-        raise NotImplementedError()
+        # table_level = self.level  # current table level
+
+        pos: int | None = None
+        kv_pair: tuple[K, V] = (None, None)
+        parent: InfiniteHashTable[K, V] | None = self
+        table_level: int = self.level
+
+        def step_into_table(table):
+            nonlocal parent, pos, kv_pair, table_level
+            parent = table
+            pos = table.hash(key)
+            kv_pair = table.array[pos]
+            table_level += 1
+
+        step_into_table(self)
+        # print('----------\n', 'setting item: ', key, ' at level', table_level, ' at pos ', pos, 'with table', kv_pair)
+
+        while kv_pair is not None:
+            if type(kv_pair[1]) == InfiniteHashTable:
+                step_into_table(kv_pair[1])
+            else:
+                # already occupied, create new table to resolve collision
+                temp = kv_pair
+                new_table = InfiniteHashTable(table_level)
+                new_table.array[new_table.hash(temp[0])] = (temp[0], temp[1])
+                # print('reinserted: ', temp[0], ' at level', table_level + 1, ' at pos ', new_table.hash(temp[0]),
+                #       'with table', new_table.array[new_table.hash(temp[0])])
+                # assign new table to parent, and set key as per instructions
+                parent.array[pos] = (key[0:min(table_level + 1, len(key))] + '*', new_table)
+                step_into_table(new_table)
+
+        # insert value
+        parent.array[pos] = (key, value)
+        self.count += 1
+        # print('set item: ', key, ' at level', table_level, ' at pos ', pos, 'with table', kv_pair)
 
     def __delitem__(self, key: K) -> None:
         """
@@ -48,10 +94,26 @@ class InfiniteHashTable(Generic[K, V]):
 
         :raises KeyError: when the key doesn't exist.
         """
-        raise NotImplementedError()
+        # try:
+        #     pos = self.get_location(key)
+        # except KeyError:
+        #     raise KeyError('Key not found')
+        # else:
+        #     parent = self
+        #     table = self
+        #     for i, p in enumerate(pos):
+        #         if i == len(pos) - 1:
+        #             table.array[p] = None
+        #             self.count -= 1
+        #         table = table.array[p][1]
+
+
 
     def __len__(self):
-        raise NotImplementedError()
+        """
+        Returns the number of elements in the hash table.
+        """
+        return self.count
 
     def __str__(self) -> str:
         """
@@ -59,7 +121,7 @@ class InfiniteHashTable(Generic[K, V]):
 
         Not required but may be a good testing tool.
         """
-        raise NotImplementedError()
+        return str(self.array)
 
     def get_location(self, key):
         """
@@ -67,7 +129,22 @@ class InfiniteHashTable(Generic[K, V]):
 
         :raises KeyError: when the key doesn't exist.
         """
-        raise NotImplementedError()
+        pos = self.hash(key)
+        kv_pair = self.array[pos]
+        path = [pos]
+
+        while kv_pair is not None:
+            if type(kv_pair[1]) == InfiniteHashTable:
+                #     print('pos is ', pos)
+                #     for i, t in enumerate(kv_pair[1].array):
+                #         if t is not None:
+                #             print(i, t)
+                pos = kv_pair[1].hash(key)
+                kv_pair = kv_pair[1].array[pos]
+                path.append(pos)
+            else:
+                return path  # found value
+        raise KeyError()  # not found
 
     def __contains__(self, key: K) -> bool:
         """
@@ -81,3 +158,15 @@ class InfiniteHashTable(Generic[K, V]):
             return False
         else:
             return True
+
+
+if __name__ == '__main__':
+    # def hashh(key, level=0):
+    #     return ord(key[level]) % (27 - 1)
+    #
+    # print(hashh('lin'))
+    # print(hashh('leg'))
+    # print(hashh('lin', 1))
+    # print(hashh('leg', 1))
+    # print('---')
+    pass
