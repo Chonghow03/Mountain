43f30d44a5a935f509725a5ecb0c34c367670275
Chonghow29 <cong0027@student.monash.edu>
2023-04-21 17:45:02 +0800

    Latest version passed task 6 and fixed key values in double key

--- double_key_table.py
+++ double_key_table.py
@@ -115,14 +115,11 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         key = None: returns all top-level keys in the table.
         key = x: returns all bottom-level keys for top-level key x.
         """
-        key_lst = []
-        if key == None:
-            for i in range(len(self.top_level_table)):
-                key_lst += self.top_level_table[i][0]
+        if key is None:
+            return self.top_level_table.keys()
         else:
-            for j in range(len(self.top_level_table[K1])):
-                key_lst += self.top_level_table[K1][1][j][0]
-        return key_lst
+            table = self.top_level_table[key]
+            return table.keys()
 
     def iter_values(self, key: K1 | None = None) -> Iterator[V]:
         """
@@ -147,14 +144,16 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         key = None: returns all values in the table.
         key = x: returns all values for top-level key x.
         """
-        value_lst = []
-        if key == None:
-            for i in range(len(self.top_level_table)):
-                value_lst += self.top_level_table[i][1]
+        if key is None:
+            # get all tables' values
+            tables = self.top_level_table.values()
+            values = []
+            for t in tables:
+                values += t.values()
+            return values
         else:
-            for j in range(len(self.top_level_table[K1])):
-                value_lst += self.top_level_table[K1][1][j][1]
-        return value_lst
+            table = self.top_level_table[key]
+            return table.values()
 
     def __contains__(self, key: tuple[K1, K2]) -> bool:
         """
